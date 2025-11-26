7cdee970aec3abd9c081e913b6e418e2e5300b1d
Ramon <Matheusramon0208@gmail.com>
2025-11-26 14:26:13 -0300

    exercicio 'cobaias' finalizado

---
+++ Estruturas Repetitivas/FOR/fatorial.py
@@ -0,0 +1,27 @@
+"""  
+Problema "fatorial" (adaptado de URI 1153)
+Fazer um programa para ler um número natural N (valor máximo: 15), e depois calcular e mostrar o
+fatorial de N.
+Exemplo 1:
+Digite o valor de N: 4
+FATORIAL = 24
+Exemplo 2:
+Digite o valor de N: 0
+FATORIAL = 1
+Exemplo 3:
+Digite o valor de N: 6
+FATORIAL = 720
+Exemplo 4:
+Digite o valor de N: 1
+FATORIAL = 1
+
+"""
+
+n = int(input("Digite o valor de N: "))
+sub = 0
+
+for i in range (1,n+1):
+    sub = sub + 1
+    n = n * (n - sub)
+    i = n + n
+    print(i)
